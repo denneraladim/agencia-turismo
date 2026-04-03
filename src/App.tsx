b5596366d0ea3273abac0655350cdffbc886e8bb
Denner Dangelo Aladim de Castro <dennerdangelo57@gmail.com>
2026-04-03 17:29:28 -0300

    Projeto Atualizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import './App.css'
-import Container from './components/Container';
 import Header from './components/Header'
 
 function App() {
    return (
     <>
       <Header />
-       <main>
-        <Container>
-          <h1>Home</h1>
-        </Container>
-      </main>
     </>
   )
 }
