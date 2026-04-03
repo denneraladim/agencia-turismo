3029090cb02a0e6657963100a4e28194f7f34f33
Denner Dangelo Aladim de Castro <dennerdangelo57@gmail.com>
2026-04-03 15:19:35 -0300

    Criado o componente Container

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
+import Container from './components/Container';
 import Header from './components/Header'
 
 function App() {
    return (
     <>
       <Header />
+       <main>
+        <Container>
+          <h1>Home</h1>
+        </Container>
+      </main>
     </>
   )
 }
---
+++ src/components/Container/index.tsx
@@ -0,0 +1,17 @@
+
+import type { ReactNode } from "react";
+import styles from "./Container.module.scss";
+
+interface ContainerProps {
+    children: ReactNode;
+}
+
+const Container = ({children}:ContainerProps) => {
+    return (
+        <div className={styles.container}>
+            {children}
+        </div>
+    )
+}
+
+export default Container;
